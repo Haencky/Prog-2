cd7c8d6cd0a71861845f34bfb7b8ba93c548a3e9
Haencky <115699809+Haencky@users.noreply.github.com>
2023-06-22 13:43:15 +0200

    Delete Uebung_9 directory

--- Uebung_9/Main.java
+++
@@ -1,9 +0,0 @@
-package Uebung_9;
-
-public class Main {
-    public static void main(String[] args) {
-        for (int i = 0; i < 100; i++) {
-            System.out.println(GenericUtil.gambling(5, 7));
-        }
-    }
-}
